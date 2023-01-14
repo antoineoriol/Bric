2505d7a2accfc2939936bd8b2ab79fd696d4357a
gueye tamsir <gueye.tamsir78@gmail.com>
2023-01-14 15:34:13 +0100

    added method in acttion

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,18 @@
 class ProductsController < ApplicationController
   def index
+    Product.all
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 end
