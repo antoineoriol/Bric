b5b078c2424404f07b7ac2ed9e77c3c985f3ffc1
antoineoriol <antoineoriol1@gmail.com>
2023-01-14 15:08:36 +0100

    second push

---
+++ db/migrate/20230114135547_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :description
+      t.integer :price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
