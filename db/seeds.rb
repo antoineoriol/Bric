e7021bfbf984f9e177c9d051ce3afc4d91291db0
Marcos Suarez <marcossuarezr88@gmail.com>
2023-01-17 19:51:32 +0100

    cloudinary pour stoker les photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-
+Product.destroy_all
+User.destroy_all
 user1 = User.create!(email: 'etest2@test.com', password: 'azerty')
 
 lego_sets = [
@@ -12,9 +13,18 @@ lego_sets = [
   { title: 'Lego voiture', description: 'Lego voiture', price: 49.99, user: user1 },
   { title: 'Lego avion', description: 'Lego avion', price: 49.99, user: user1 },
   { title: 'Lego train', description: 'Lego train', price: 49.99, user: user1 },
-  { title: 'Lego bateau', description: 'Lego bateau', price: 49.99, user: user1 },
+  { title: 'Lego bateau', description: 'Lego bateau', price: 49.99, user: user1 }
 ]
 
+#lego_photo = ['star_wars', 'eiffel', 'piramide']
+
+file = File.open("app/assets/images/star_wars.jpg")
+
 lego_sets.each do |lego_set|
-  Product.create!(lego_set)
+  product = Product.create!(
+    title: lego_set['title'],
+    description: lego_set['description']
+  )
+    product.photo.attach(io: file, filename: "lego", content_type: "image/jpg")
+    product.save
 end
